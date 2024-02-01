5ede2a85347d6778f5252e747540eaf12e74a2ea
morpha <v21.leonardo@gmail.com>
2024-02-01 15:30:51 -0300

    moved some stuff from python_scripts/main.py to python_scripts/extract/chess_request.py

--- python_scripts/extract/chess_request.py
+++ python_scripts/extract/chess_request.py
@@ -4,20 +4,35 @@ this module only provides functions to request data from a chess API
 
 
 import requests as rq
+import pandas as pd
+from json_normalize import json_normalize
+
+def request_games(year: int, month: int, user: str, email: str) -> dict:
+	"""gets chess data from chess.com"""
+	year  = str(year)
+	month = ("0"+str(month))[-2:]
+	url   = f"https://api.chess.com/pub/player/{user}/games/{year}/{month}"
+	print("requesting game data... ")
+	headers = {'User-Agent': email}
+	response = rq.get(url, headers=headers)
+
+	if response.status_code == 200:
+		game_json = response.json()
+		df = pd.DataFrame(list(json_normalize(game_json)))
+		print("done.\n")
+		return df
+	else:
+		print(f'Error: {response.status_code}')
+		return None
 
 
-def get_games(url: str, email: str) -> dict:
-  """gets chess data from chess.com"""
-  print("requesting game data... ")
-  headers = {'User-Agent': email}
-  response = rq.get(url, headers=headers)
 
-  if response.status_code == 200:
-    print("done.\n")
-    return response.json()
-  else:
-    print(f'Error: {response.status_code}')
-    return None
+
+
+
+
+
+
 
 
 
--- python_scripts/main.py
+++ python_scripts/main.py
@@ -1,27 +1,27 @@
 import pandas as pd
-from json_normalize import json_normalize
 
 
-from extract.chess_request import get_games
+from extract.chess_request import request_games
 import transform.chess_transform as ct
 
+year  = 2024
+month = 1
+user  = "morpha_21"
+email = ""
 
 
-
-with open(".email", 'r') as email_file:
-	url	  = "https://api.chess.com/pub/player/morpha_21/games/2019/12"
+with open("extract/.email", 'r') as email_file:
 	email	  = email_file.read().strip()
-	game_json = get_games(url, email)['games']
-	df        = pd.DataFrame(list(json_normalize(game_json)))
 
 
+df = request_games(year, month, user, email)
 df = ct.adequate(df)
 
 for i in df.columns:
 	print(i)
 
 print()
-print(df.iloc[:, 15:20])
+print(df)
 
 
 
