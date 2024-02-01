3dc38e2704ead60ca6ab22054113983cba9ecc36
morpha <v21.leonardo@gmail.com>
2024-02-01 14:46:57 -0300

    fixed column droping

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -6,12 +6,10 @@ from extract.chess_request import get_games
 import transform.chess_transform as ct
 
 
-# maybe getting user from file?
-# dates as arguments?
 
 
 with open(".email", 'r') as email_file:
-	url	  = "https://api.chess.com/pub/player/morpha_21/games/2023/08"
+	url	  = "https://api.chess.com/pub/player/morpha_21/games/2019/12"
 	email	  = email_file.read().strip()
 	game_json = get_games(url, email)['games']
 	df        = pd.DataFrame(list(json_normalize(game_json)))
--- python_scripts/transform/chess_transform.py
+++ python_scripts/transform/chess_transform.py
@@ -6,10 +6,10 @@ def format_columns(columns: pd.core.indexes.base.Index) -> list:
 	return [col.replace(".","_").replace("@","") for col in columns]
 
 
-columns_to_be_dropped = ['initial_setup', 'rated', 'rules', 'white_id', 'black_id',
+disposable_columns = ['initial_setup', 'rated', 'rules', 'white_id', 'black_id',
 			   'start_time']
 
 
 def adequate(df: pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
 	df.columns = format_columns(df.columns)
-	return df.drop(columns_to_be_dropped, axis=1)
+	return df.drop([col for col in disposable_columns if col in df.columns], axis=1)
