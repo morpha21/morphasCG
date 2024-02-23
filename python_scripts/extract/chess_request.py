29f4d4d2e94960cec11c63a9969cf5823b094c39
morpha <v21.leonardo@gmail.com>
2024-02-23 01:17:18 -0300

    Able to create a nice DataFrame, implementation of personalize function in transform/chess_transform.py

--- python_scripts/extract/chess_request.py
+++ python_scripts/extract/chess_request.py
@@ -7,19 +7,18 @@ import requests as rq
 import pandas as pd
 from json_normalize import json_normalize
 
-def request_games(year: int, month: int, user: str, email: str) -> dict:
+def request_games(year: int, month: int, user: str, email: str) -> pd.core.frame.DataFrame:
 	"""gets chess data from chess.com"""
 	year  = str(year)
 	month = ("0"+str(month))[-2:]
 	url   = f"https://api.chess.com/pub/player/{user}/games/{year}/{month}"
-	print("requesting game data... ")
+	print(f"\rrequesting game data from {month}/{year}... ", end=' ')
 	headers = {'User-Agent': email}
 	response = rq.get(url, headers=headers)
 
 	if response.status_code == 200:
 		game_json = response.json()
 		df = pd.DataFrame(list(json_normalize(game_json)))
-		print("done.\n")
 		return df
 	else:
 		print(f'Error: {response.status_code}')
--- python_scripts/main.py
+++ python_scripts/main.py
@@ -1,11 +1,12 @@
+from datetime import datetime
 import pandas as pd
-
 
 from extract.chess_request import request_games
 import transform.chess_transform as ct
 
-year  = 2024
-month = 1
+
+current_year = datetime.now().year
+current_month = datetime.now().month
 user  = "morpha_21"
 email = ""
 
@@ -14,9 +15,28 @@ with open("extract/.email", 'r') as email_file:
 	email = email_file.read().strip()
 
 
-df = request_games(year, month, user, email)
-df = ct.adequate(df)
+df_list = []
 
+for year in range(2023, current_year+1):
+	for month in range(1, 13):
+		df = request_games(year, month, user, email)
+		df_list += [df]
+		if (year == current_year) and (month == current_month):
+			break
+
+df = pd.concat(df_list)
+df = ct.adequate(df)
+df.sort_index(inplace=True)
+
+uuid = df[df['white_username'] == user]['white_uuid'].unique()[0]
+
+
+df = ct.personalize(df, user)
+
+
+
+
+print("\nColumns:")
 for i in df.columns:
 	print(i)
 
