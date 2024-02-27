65c330ed59045b07aaa8acf151b1a7ca0d5c01a0
morpha <v21.leonardo@gmail.com>
2024-02-27 16:07:10 -0300

    renamed main.py to load_to_csv.py, and it now receives user, argument e and date as arguments from command line

--- python_scripts/load_to_csv.py
+++ python_scripts/load_to_csv.py
@@ -1,4 +1,15 @@
 from sys import argv
+if len(argv) == 1:
+	print("usage:")
+	print("python load_to_csv <chess.com's player username> <start_date> <end_date>")
+	print("")
+	print("both <start_date> and <end_date> should in the format YYYY/mm")
+	print("both <start_date> and <end_date> are optional with the following default values:")
+	print("<start_date> = 2015/01")
+	print("<end_date>   = current month")
+	print("")
+	print("If only one date is specified, it will be considered as <start_date>")
+	exit()
 
 
 import concurrent.futures
@@ -13,20 +24,24 @@ from extract.chess_request import get_user
 
 def worker(year_,user_,email_):
 	df_list = []
-	current_month = datetime.now().month
-	global current_year
+	global end_date
+	global start_date
 
 	for month in range(1, 13):
+		if (year == start_date.year) and (month < start_date.month):
+			continue
+
 		df = request_games(year_, month, user_, email_)
 		df_list += [df]
-		if (year_ == current_year) and (month == current_month):
+		
+		if (year_ == end_date.year) and (month == end_date.month):
 			break
 	return pd.concat(df_list)
 
 
-
-current_year = datetime.now().year
-user         = "morpha_21"
+user       = argv[1]
+start_date = datetime.strptime(argv[2], "%Y/%m")
+end_date = datetime.strptime(argv[3], "%Y/%m") if len(argv) > 3 else datetime.now()
 email        = ""
 
 with open("extract/.email", 'r') as email_file:
@@ -34,7 +49,7 @@ with open("extract/.email", 'r') as email_file:
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
 	futures = []
-	for year in range(2023, current_year+1):
+	for year in range(start_date.year, end_date.year+1):
 			futures += [executor.submit(worker, year, user, email)]
 	df_list = [f.result() for f in futures]
 
